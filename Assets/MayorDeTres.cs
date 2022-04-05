24b0ce7585bc0b3dadaed48508a87901254ea800
ambarpalermo <ambarpalermo06@gmail.com>
2022-04-05 08:42:39 -0300

    Initial commit

---
+++ Assets/MayorDeTres.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MayorDeTres : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("hola"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
